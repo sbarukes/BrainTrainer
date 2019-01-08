2a585732aeb8c209d43f31193d300c471c9a0669
sbarukes <sbarukes@gmail.com>
2019-01-07 21:50:55 -0500

    Initial commit - Building UI

---
+++ app/src/main/java/com/example/austin/braintrainer/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.austin.braintrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button goButton;
+
+    public void start(View view){
+        goButton.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        goButton = findViewById(R.id.goButton);
+    }
+}
