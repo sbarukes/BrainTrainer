c57e020cfc3c1c883fd4913416c06a46db379cc2
sbarukes <sbarukes@gmail.com>
2019-01-09 15:12:32 -0500

    Added Random Sum TextView

--- app/src/main/java/com/example/austin/braintrainer/MainActivity.java
+++ app/src/main/java/com/example/austin/braintrainer/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView sumTextView = findViewById(R.id.sumTextView);
         goButton = findViewById(R.id.goButton);
+
+        Random rand = new Random();
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
     }
 }
